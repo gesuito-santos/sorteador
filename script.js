bf2dbd8eb2b96e167e53e5b982b7f3371e9f2e1c
gesuito-santos <ge.xp0210@gmail.com>
2024-07-09 14:46:57 -0300

    update script

--- script.js
+++ script.js
@@ -3,12 +3,18 @@ function generateNumber() {
     const min = Math.ceil(document.querySelector(".input-min").value)
     const max = Math.floor(document.querySelector(".input-max").value)
 
+    const numero = document.querySelector(".numero")
+
+    if (max > min) {
 
         const result = Math.floor(Math.random() * (max - min + 1) + min);
-        const numero = document.querySelector(".numero")
 
         numero.innerHTML = result
-    
-  }
+        
+    } else {
+        alert("O valor mínimo deve ser MENOR que o valor máximo!")
+    }
+
+}
 
 
